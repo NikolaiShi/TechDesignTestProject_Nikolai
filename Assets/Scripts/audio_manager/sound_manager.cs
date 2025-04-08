27d2c9ee1f4eeb33e783223473cf8d0483d05ffb
NikolaiShidlovski <nikolai.shidlovski@gmail.com>
2025-04-08 04:15:36 +0300

    added audio manager and audio assets

---
+++ Assets/Scripts/audio_manager/sound_manager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class sound_manager : MonoBehaviour
+{
+    public static sound_manager Instance;
+
+    [SerializeField]
+    private sound_library sfxLibrary;
+    [SerializeField]
+    private AudioSource sfx2DSource;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void PlaySound3D(AudioClip clip, Vector3 pos)
+    {
+        if (clip != null)
+        {
+            AudioSource.PlayClipAtPoint(clip, pos);
+        }
+    }
+
+    public void PlaySound3D(string soundName, Vector3 pos)
+    {
+        PlaySound3D(sfxLibrary.GetClipFromName(soundName), pos);
+    }
+
+    public void PlaySound2D(string soundName)
+    {
+        sfx2DSource.PlayOneShot(sfxLibrary.GetClipFromName(soundName));
+    }
+}
